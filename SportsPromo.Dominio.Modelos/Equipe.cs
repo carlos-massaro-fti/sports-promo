b763b973b3b1b9e081eb8e8cabf3d3fb4084a10a
Allanhenriquee <52016301+Allanhenriquee@users.noreply.github.com>
2019-08-12 19:37:50 -0300

    Classe equipe

---
+++ SportsPromo.Dominio.Modelos/Equipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPromo.Dominio.Modelos
+{
+    class Equipe
+    {
+        public long EquipeID { get; set; }
+
+        public string EquipeNome { get; set; }
+        [ForeignKey("IdProva")]
+        public long EquipeProvaID { get; set; }
+    }
+}
