4e4a5a155e13f83c1b3010f45e98887d0eabc682
Carlos Massaro <carlos.massaro@ftitecnologia.com.br>
2019-08-12 19:16:31 -0300

    Classe Esporte

---
+++ SportsPromo.Dominio.Modelos/Esporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPromo.Dominio.Modelos
+{
+    public class Esporte
+    {
+        public long EsporteId { get; set; }
+        public string EsporteNome { get; set; }
+    }
+}
