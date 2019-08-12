f81de109af7a38820bdfd28fe5762dbbc86a4f01
Barbara <bferrucior@gmail.com>
2019-08-12 19:52:15 -0300

    Adiciona Marco e alterações.

---
+++ SportsPromo.Dominio.Modelos/Marco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPromo.Dominio.Modelos
+{
+    public class Marco
+    {
+        public long MarcoID { get; set; }
+        public decimal MarcoLat { get; set; }
+        public decimal MarcoLon { get; set; }
+
+        [ForeignKey("Prova")]
+        public long MarcoProvaID { get; set; }
+        public Prova Prova { get; set; }
+
+        [ForeignKey("Receptor")]
+        public long MarcoReceptorID { set; get; }
+        public Receptor Receptor { set; get;  }
+
+    }
+}
