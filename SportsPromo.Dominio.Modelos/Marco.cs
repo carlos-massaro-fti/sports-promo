b659a765e9a017e8ec919ce48bb4629215aeb3b6
rafael ozelin de assuncao <aditiv3@hotmail.com>
2019-08-12 19:55:44 -0300

    model marco

---
+++ SportsPromo.Dominio.Modelos/Marco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPromo.Dominio.Modelos
+{
+    public class Marco
+    {
+        public int MarcoId { get; set; }
+
+        public decimal MarcoLat { get; set; }
+
+        public decimal MarcoLon { get; set; }
+
+        //public Prova Prova { get; set; }
+
+        public int MarcoProvaId { get; set; }
+
+        public Receptor Receptor { get; set; }
+
+        public int MarcoReceptorId { get; set; }
+    }
+}
