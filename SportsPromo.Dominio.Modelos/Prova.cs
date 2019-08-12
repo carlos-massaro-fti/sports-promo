85aee3d31d508d489d032cad3b65e59d5e17d054
Eder Silva <eder.92.silva@hotmail.com>
2019-08-12 20:24:10 -0300

    Criação da classe Prova

--- SportsPromo.Dominio.Modelos/Prova.cs
+++ SportsPromo.Dominio.Modelos/Prova.cs
@@ -1,6 +1,16 @@
-﻿namespace SportsPromo.Dominio.Modelos
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsPromo.Dominio.Modelos
 {
     public class Prova
     {
+        public long ProvaId { get; set; }
+        [ForeignKey("Esporte")]
+        public long EsporteId { get; set; }
+        public DateTime ProvaComecaEm { get; set; }
+        public string Evento_Id { get; set; }
+        [ForeignKey("Evento")]
+        public long ProvaEvento { get; set; }
     }
 }
