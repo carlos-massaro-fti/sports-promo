5529d7bd7eeacbd0eddc5d2ddb953146f3cd5851
rafael ozelin de assuncao <aditiv3@hotmail.com>
2019-08-12 19:43:25 -0300

    model usuario

---
+++ SportsPromo.Dominio.Modelos/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPromo.Dominio.Modelos
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        public string UsuarioNome { get; set; }
+
+        public string UsuarioEmail { get; set; }
+
+        public bool UsuarioEhAtivado { get; set; }
+
+        public string UsuarioSenha { get; set; }
+    }
+}
