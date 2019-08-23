e2296d4bfd4bd3cc451aab968363c5c7d8458198
Allanhenriquee <52016301+Allanhenriquee@users.noreply.github.com>
2019-08-22 21:06:23 -0300

    Wallace

---
+++ SportsPromo.Dados.Repositorios/CategoriaRepositorio.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using SportsPromo.Comum.Dados;
+using SportsPromo.Dominio.Modelos;
+using SportsPromo.Interfaces.Dados.Contextos;
+using SportsPromo.Interfaces.Dados.Repositorios;
+namespace SportsPromo.Dados.Repositorios
+{
+    public class CategoriaRepositorio : BaseRepositorio<Categoria> , ICategoriaRepositorio
+    {
+        public CategoriaRepositorio(ISportsPromoContexto contexto) : base(contexto)
+        {
+        }
+
+        public override long Adicionar(Categoria instancia)
+        {
+            Contexto.Categorias.Add(instancia);
+
+            Contexto.SaveChanges();
+
+            return instancia.CategoriaId;
+        }
+
+        public Task<long> AdicionarAsync(Categoria instancia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AlterarAsync(Categoria instancia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletarAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaginadoOrdenado<Categoria> Listar(PaginadoOrdenado<Categoria> consulta)
+        {
+            var resultado = new PaginadoOrdenado<Categoria>()
+            {
+                ItensPorPagina = consulta.ItensPorPagina,
+                PaginaAtual = consulta.PaginaAtual,
+                OrdemDirecao = consulta.OrdemDirecao,
+                OrdemNome = consulta.OrdemNome,
+            };
+
+            /*var query = from item in Contexto.Esportes
+                        select item;*/
+
+            var query = Contexto.Categorias.AsQueryable();
+
+
+            resultado.SetTotalDeLinhas(query.Count());
+
+            if (consulta.OrdemDirecao == 0)
+            {
+                switch (consulta.OrdemNome)
+                {
+                    case "CategoriaNome":
+                        query = query.OrderBy(e => e.CategoriaNome);
+                        break;
+                    case "CategoriaId":
+                    default:
+                        query = query.OrderBy(e => e.CategoriaId);
+                        break;
+                }
+            }
+            else
+            {
+                switch (consulta.OrdemNome)
+                {
+                    case "CategoriaNome":
+                        query = query.OrderByDescending(e => e.CategoriaNome);
+                        break;
+                    case "CategoriaId":
+                    default:
+                        query = query.OrderByDescending(e => e.CategoriaId);
+                        break;
+                }
+            }
+            if (!(query is IOrderedQueryable))
+            {
+                query = query.OrderBy(e => e.CategoriaId);
+            }
+
+            query = query
+                .Skip(resultado.ItensPorPagina * (resultado.PaginaAtual - 1))
+                .Take(resultado.ItensPorPagina);
+
+            resultado.Itens = query;
+
+            return resultado;
+        }
+
+        public async Task<PaginadoOrdenado<Categoria>> ListarAsync(PaginadoOrdenado<Categoria> consulta)
+        {
+            var resultado = new PaginadoOrdenado<Categoria>()
+            {
+                ItensPorPagina = consulta.ItensPorPagina,
+                PaginaAtual = consulta.PaginaAtual,
+                OrdemDirecao = consulta.OrdemDirecao,
+                OrdemNome = consulta.OrdemNome,
+            };
+
+            /*var query = from item in Contexto.Esportes
+                        select item;*/
+
+            var query = Contexto.Categorias.AsQueryable();
+
+
+            resultado.SetTotalDeLinhas(await query.CountAsync());
+
+            if (consulta.OrdemDirecao == 0)
+            {
+                switch (consulta.OrdemNome)
+                {
+                    case "CategoriaNome":
+                        query = query.OrderBy(e => e.CategoriaNome);
+                        break;
+                    case "CategoriaId":
+                    default:
+                        query = query.OrderBy(e => e.CategoriaId);
+                        break;
+                }
+            }
+            else
+            {
+                switch (consulta.OrdemNome)
+                {
+                    case "CategoriaNome":
+                        query = query.OrderByDescending(e => e.CategoriaNome);
+                        break;
+                    case "CategoriaId":
+                    default:
+                        query = query.OrderByDescending(e => e.CategoriaId);
+                        break;
+                }
+            }
+            if (!(query is IOrderedQueryable))
+            {
+                query = query.OrderBy(e => e.CategoriaId);
+            }
+
+            query = query
+                .Skip(resultado.ItensPorPagina * (resultado.PaginaAtual - 1))
+                .Take(resultado.ItensPorPagina);
+
+            resultado.Itens = query;
+
+            return resultado;
+        }
+
+        public Task<List<Categoria>> ListarAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> PegarAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SportsPromo.Dominio.Servicos/CategoriaServico.cs
@@ -0,0 +1,142 @@
+﻿using SportsPromo.Interfaces.Dados.Repositorios;
+using SportsPromo.Interfaces.Dominio.Servicos;
+using SportsPromo.Dominio.Modelos;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsPromo.Comum.Exceptions;
+using SportsPromo.Comum.Dados;
+
+namespace SportsPromo.Dominio.Servicos
+{
+    public class CategoriaServico : ICategoriaServico
+    {
+        public ICategoriaRepositorio CategoriaRepositorio { get; }
+        public CategoriaServico(ICategoriaRepositorio categoriaRepositorio)
+        {
+            CategoriaRepositorio = categoriaRepositorio;
+        }
+
+        public long Adicionar(Categoria instancia)
+        {
+            var validationResult = Validar(instancia);
+
+            if (validationResult.Any())
+            {
+                throw new AppException(validationResult.First().ErrorMessage, validationResult);
+            }
+
+            var result = CategoriaRepositorio.Adicionar(instancia);
+
+            return result;
+        }
+
+        public bool Alterar(Categoria instancia)
+        {
+            var atualInstancia = CategoriaRepositorio.Pegar(instancia.CategoriaId);
+
+            Mesclar(atualInstancia, instancia);
+
+            var validationResult = Validar(atualInstancia);
+
+            if (validationResult.Any())
+            {
+                throw new AppException(validationResult.First().ErrorMessage, validationResult);
+            }
+
+            var result = CategoriaRepositorio.Alterar(atualInstancia);
+
+            return result;
+        }
+        private void Mesclar(Categoria atualInstancia, Categoria novaInstancia)
+        {
+            atualInstancia.CategoriaNome = novaInstancia.CategoriaNome;
+        }
+
+
+        public bool Deletar(long id)
+        {
+            var result = CategoriaRepositorio.Deletar(id);
+
+            return result;
+        }
+
+        public PaginadoOrdenado<Categoria> Listar(PaginadoOrdenado<Categoria> consulta)
+        {
+            var result = CategoriaRepositorio.Listar(consulta);
+
+            return result;
+        }
+        public List<Categoria> Listar()
+        {
+            var result = CategoriaRepositorio.Listar();
+
+            return result;
+        }
+
+        public Categoria Pegar(long id)
+        {
+            var result = CategoriaRepositorio.Pegar(id);
+
+            return result;
+        }
+
+        public IEnumerable<ValidationResult> Validar(Categoria instancia)
+        {
+            if (instancia.CategoriaNome.Length > 256)
+            {
+                yield return new ValidationResult("O Nome não pode ter mais de 256 caracteres", new string[] { "CategoriaNome" });
+            }
+            if (instancia.CategoriaNome.Length < 3)
+            {
+                yield return new ValidationResult("O Nome não pode ter menos de 3 caracteres", new string[] { "CategoriaNome" });
+            }
+
+            if (instancia.CategoriaIdadeMin > instancia.CategoriaIdadeMax)
+            {
+                yield return new ValidationResult("A idade minima não pode ser menor que a idade maxima", new string[] { "CategoriaIdadeMin", "CategoriaIdadeMax" });
+            }
+
+            if (instancia.CategoriaGeneroId > 0)
+            {
+                yield return new ValidationResult("Favor selecionar um genero", new string[] { "CategoriaGeneroId" });
+            }
+        }
+
+        public Task<IEnumerable<ValidationResult>> ValidarAsync(Categoria instancia)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<long> AdicionarAsync(Categoria instancia)
+        {
+            var resultado = await CategoriaRepositorio.AdicionarAsync(instancia);
+
+            return resultado;
+        }
+        public Task<bool> AlterarAsync(Categoria instancia)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeletarAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<PaginadoOrdenado<Categoria>> ListarAsync(PaginadoOrdenado<Categoria> consulta)
+        {
+            var resultado = await CategoriaRepositorio.ListarAsync(consulta);
+
+            return resultado;
+        }
+        public Task<List<Categoria>> ListarAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Categoria> PegarAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
